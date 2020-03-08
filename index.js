48d33ec2a518dac21c47d4e3b3cccbdfe438b74c
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-03-08 16:22:00 +0500

    Lecture 12 - Assignments
    This is to remind you about the assignment.

---
+++ index.js
@@ -0,0 +1,22 @@
+// var yourName = prompt("Please enter your Name:");
+// var batchName = "MERN Stack";
+// batchName = "MERN Stack Batch";
+// console.log("Welcome " + yourName + ", to " + batchName + " Batch.");
+
+// Q1: Your dogs age is 4 years which equals 16 years in human age
+
+var myName = "Jahanzeb Naeem";
+// myName.length;
+// myName.slice(5,8);
+// myName.toLowerCase();
+// myName.toUpperCase();
+
+// Q2: jahanzeb JAHANZEB Jahanzeb jaHANZEB = Show Jahanzeb
+
+function calValue () {
+    console.log(2*2);
+}
+
+calValue();
+
+// Q3 and Q4 as per discussed in the class.
