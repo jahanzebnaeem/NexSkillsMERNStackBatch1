89eb8644f675c19e1c05159342ddd0f6309225a6
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-03-14 17:05:16 +0500

    Lecture 13
    JavaScript Intermediate

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
 // Q1: Your dogs age is 4 years which equals 16 years in human age
 
-var myName = "Jahanzeb Naeem";
+// var myName = "Jahanzeb Naeem";
 // myName.length;
 // myName.slice(5,8);
 // myName.toLowerCase();
@@ -13,10 +13,57 @@ var myName = "Jahanzeb Naeem";
 
 // Q2: jahanzeb JAHANZEB Jahanzeb jaHANZEB = Show Jahanzeb
 
-function calValue () {
-    console.log(2*2);
-}
+// function calValue () {
+//     console.log(2*2);
+// }
 
-calValue();
+// calValue();
 
 // Q3 and Q4 as per discussed in the class.
+
+// 14/03/2020
+
+// function howManyBreads(amount) {
+//     return numberOfBreads = Math.floor(amount/100);
+// }
+
+// howManyBreads(350);
+
+// function howManyBreads(amount) {
+//     return numberOfBreads = Math.floor(amount%100);
+// }
+
+// howManyBreads(350);
+
+// Math.floor((Math.random() * 6) + 1);
+
+// Q1: Create Love Calculator
+
+/* Message in alert 
+    if % less than 30. Sorry find someone else.
+    if % less than 70. Need to try some more.
+    if % less than 95. You are compatible.
+    if % 100 than. This is your true love.
+*/
+// Q2: Make lodo logic
+// if consecutive 3 6's then Sorry, better luck next time.
+
+// if (lovePercent === 100) {
+//     alert("This is your true love.");
+// }
+// else if (lovePercent <= 95) {
+//     alert("You are compatible.")
+// }
+// else {
+//     alert("Sorry, find someone else.")
+// }
+
+// var i = 0;
+// while (i<10) {
+//     console.log(i);
+//     i++;
+// }
+
+// for (var i = 0; i<10; i++) {
+//     console.log(i);
+// }
