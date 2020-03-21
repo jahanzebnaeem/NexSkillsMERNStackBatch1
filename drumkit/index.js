b3218cd51a116bf50239f876f752b477628b2beb
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-03-21 15:05:31 +0500

    Updated file
    Updated the js file to understand annonomus function working.

---
+++ drumkit/index.js
@@ -0,0 +1,13 @@
+var numberOfInstuments = document.querySelectorAll(".drum").length;
+//var drumValues = document.querySelectorAll(".drum");
+// for(var i=0; i<numberOfInstuments; i++) {
+//    document.querySelectorAll(".drum")[i].style.color = "red";
+//  }
+
+document.querySelectorAll(".drum")[0].addEventListener("click", function () {
+  alert("I was pressed");
+});
+
+// function pressButton() {
+//   alert("I was pressed");
+// }
