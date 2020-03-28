81b2585f078acbec7bf3e89109329bb4d95ebddd
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-03-28 19:14:28 +0500

    Lecture 17 - Final
    The complete code of the class work.

---
+++ nodeIntro/index.js
@@ -0,0 +1,11 @@
+// console.log("Welcome to Node Intro Class");
+
+// const fs = require("fs");
+//
+// fs.copyFileSync("text1.txt", "test1.txt");
+
+const superheroes = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
